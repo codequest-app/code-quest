2a0db12d9de10984ed21af2227d49844df6af540
recca0120 <recca0120@gmail.com>
2026-05-09 10:43:53 +0800

    feat: bundle Node.js 22 binary in release, start scripts use bundled node
    - Copy node binary from GitHub Actions runner into release package
    - start.sh/start.bat prefer bundled ./node, fallback to system node
    - Users no longer need to install Node.js

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,17 +1,10 @@
 @echo off
-cd /d "%~dp0server"
-
-:: Rebuild better-sqlite3 if Node.js version doesn't match
-node -e "try{require('./node_modules/better-sqlite3/build/Release/better_sqlite3.node')}catch(e){if(e.code==='ERR_DLOPEN_FAILED'){process.exit(1)}}" 2>nul
-if errorlevel 1 (
-  echo Rebuilding better-sqlite3 for current Node.js version...
-  cd node_modules\better-sqlite3
-  npx --yes prebuild-install 2>nul || npx --yes node-gyp rebuild
-  cd ..\..
-)
-
+set "DIR=%~dp0"
+set "NODE=%DIR%node.exe"
+if not exist "%NODE%" set "NODE=node"
+cd /d "%DIR%server"
 if exist .env (
-  node --env-file=.env bin\server.js %*
+  "%NODE%" --env-file=.env bin\server.js %*
 ) else (
-  node bin\server.js %*
+  "%NODE%" bin\server.js %*
 )
